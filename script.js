735c98e0cd5715a8f71b3d6c94eacbfc81819fb5
Santino <122167715+Ssantinoo@users.noreply.github.com>
2026-08-18 17:45:32 +0000

    Add play a round, scores, variables

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3) + 1;
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() { 
+    const computerChoice = Math.floor(Math.random() * 3) + 1;
     
-    if (choice === 1) {
+    if (computerChoice === 1) {
         return "Rock";
-    } else if (choice === 2) {
+    } else if (computerChoice === 2) {
         return "Paper";
     } else {
         return "Scissors";
@@ -11,7 +14,7 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Enter Rock, Paper or Scissors: ").toLowerCase();
+    const humanChoice = prompt("Enter Rock, Paper or Scissors: ").toLowerCase();
 
     if (humanChoice === "rock") {
         return "Rock";
@@ -24,5 +27,35 @@ function getHumanChoice() {
     }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        humanScore++;
+        return "You win! Rock beats Scissors";
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        humanScore++;
+        return "You win! Paper beats Rock";
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        humanScore++;
+        return "You win! Scissors beats Paper";
+    } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+        computerScore++;
+        return "You lose! Rock beats Scissors";
+    } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+        computerScore++;
+        return "You lose! Paper beats Rock";
+    } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+        computerScore++;
+        return "You lose! Scissors beats Paper";
+    } else if (humanChoice === computerChoice) {
+        return "It's a tie";
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(`Your selection: ${humanSelection}`);
+console.log(`Computer selection: ${computerSelection}`);
+console.log(playRound(humanSelection, computerSelection));
+console.log(`Human score: ${humanScore}`);
+console.log(`Computer score: ${computerScore}`);
